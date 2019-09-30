fca3430e179f8dc35f8153652e8e72514a273826
Tyler Spreen <tspree01@users.noreply.github.com>
2019-09-30 13:23:44 -0500

    changed to a foor loop for self join on the frequent items

--- src/main/java/sample/AssociationRule.java
+++ src/main/java/sample/AssociationRule.java
@@ -6,6 +6,7 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.rdd.RDD;
 import scala.Tuple2;
+import scala.Tuple3;
 import shapeless.Tuple;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class AssociationRule
 {
 	public static void main(String[] args) throws Exception
 	{
+		List<Tuple2<Tuple2<String,String>,Integer>> doubleItemSet = new ArrayList<>();
 		SparkConf conf = new SparkConf().setAppName("WordCount").setMaster("local");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaRDD<String> lines = sc.textFile("browsingg.txt");
@@ -24,13 +26,24 @@ public class AssociationRule
 		JavaPairRDD<String, Integer> singleItemsetAfterPruning = singleItemsetBeforePruning.reduceByKey((n1, n2) -> n1 + n2).filter(l -> l._2 < 100);
 		JavaPairRDD<String, Integer> singleItemsetAfterPrunings = singleItemsetBeforePruning.reduceByKey((n1, n2) -> n1 + n2).filter(l -> l._2 >= 100);
 
-		//JavaRDD<String> singleItemSetFrequentItems = products.subtract(singleItemsetAfterPruning.keys());
+		JavaRDD<String> singleItemSetFrequentItems = singleItemsetAfterPruning.keys();
+		for (int i = 0; i < singleItemsetAfterPrunings.count(); i++)
+		{
+			for (int j = 0; j < singleItemsetAfterPrunings.count(); j++)
+			{
+				if (! singleItemsetAfterPrunings.take((int) singleItemsetAfterPrunings.count()).get(i)._1.equals(singleItemsetAfterPrunings.take((int) singleItemsetAfterPrunings.count()).get(j)._1))
+				{
+					doubleItemSet.add(new Tuple2<>(new Tuple2<>(singleItemsetAfterPrunings.take((int) singleItemsetAfterPrunings.count()).get(i)._1, singleItemsetAfterPrunings.take((int) singleItemsetAfterPrunings.count()).get(j)._1),singleItemsetAfterPrunings.take((int) singleItemsetAfterPrunings.count()).get(i)._2 *singleItemsetAfterPrunings.take((int) singleItemsetAfterPrunings.count()).get(j)._2));
+				}
+			}
+		}
 
 		//JavaPairRDD<String, String> doubleItemSet = singleItemSetFrequentItems.cartesian(singleItemSetFrequentItems).filter(l -> !l._2.equals(l._1));
-
-		JavaPairRDD<Tuple2<String, String>, Integer> doubleItemSetPairs = singleItemsetAfterPrunings.mapToPair(w -> new Tuple2<>(new Tuple2<>(w._1, w._1), w._2 * w._2));
-		JavaPairRDD<Tuple2<String, String>, Integer> doubleItemSetCount = doubleItemSetPairs.reduceByKey(Integer::sum);
+		JavaPairRDD<Tuple2<String, String>, Integer> doubleItemSetFrequentItems = sc.parallelizePairs(doubleItemSet);
+		//JavaPairRDD<Tuple2<String, String>, Integer> doubleItemSetPairs = doubleItemSetFrequentItems.mapToPair(w -> new Tuple2<>(new Tuple2<>(w._1, w._2), 1));
+		JavaPairRDD<Tuple2<String, String>, Integer> doubleItemSetCount = doubleItemSetFrequentItems.reduceByKey(Integer::sum);
 		singleItemsetAfterPrunings.saveAsTextFile("Outputs.txt");
+		doubleItemSetFrequentItems.saveAsTextFile("Outputss.txt");
 		doubleItemSetCount.saveAsTextFile("Output.txt");
 
 		sc.stop();
